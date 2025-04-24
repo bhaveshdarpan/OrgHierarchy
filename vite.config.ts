070642d43da665df4da6423bff24144b3c973e60
Bhavesh Darpan <tapsankavyes@gmail.com>
2025-04-25 01:57:20 +0530

    deployment stuff

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/org-hierarchy-frontend/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
